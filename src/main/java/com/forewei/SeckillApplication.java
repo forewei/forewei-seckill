5de65b8164996194caaa0ed1c05933bd5d0108e5
forewei <597082933@qq.com>
2019-05-21 18:00:38 +0800

    init

---
+++ src/main/java/com/forewei/SeckillApplication.java
@@ -0,0 +1,23 @@
+package com.forewei;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+/**
+ * @Author forewei
+ * @date 2019-5-21 17:27
+ */
+@SpringBootApplication
+@ComponentScan("com.forewei")
+public class SeckillApplication {
+	private final static Logger LOGGER = LoggerFactory.getLogger(SeckillApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(SeckillApplication.class, args);
+		LOGGER.info("项目启动");
+	}
+
+}
---
+++ src/main/java/com/forewei/config/RedisConfig.java
@@ -0,0 +1,12 @@
+package com.forewei.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author forewei
+ * @date 2019-5-21 17:27
+ */
+@Configuration
+public class RedisConfig {
+
+}
